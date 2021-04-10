c29a20f99320c27042ebb82120b5adda51d5fd57
vmvndv <44955320+vmvndv@users.noreply.github.com>
2021-04-10 15:41:22 -0400

    Update scheduler, requirements.txt

--- relBasedScheduling.py
+++ relBasedScheduling.py
@@ -1,10 +1,8 @@
 # input = n data queries, n event patterns, m event relations
 # output: event ID tuples that satisfy all constraints
 
-# pruning score computed for every event based on the number of constraints specified
 # event relationships are sorted based on the relationship type (process events and network events) and the sum of the involved event pattern's pruning scores
 # main loop processes event relationships returned from the sorted list, executes data queries, and generates result tuples
-# we maintain a map M that stores the mapping from the event pattern ID to the set of event ID tuples that its execution results belong to. 
 # As the loop continues, new tuple sets are created and put into M, and old tuple sets are updated, filtered, or merged.
 # after all event relationships, any unexecuted queries are executed and results are put into M
 # merge tuples in M so all event patterns are mapped to the same tuple set that satisfy all constraints 
@@ -13,7 +11,7 @@ import collections
 import operator
 import nltk
 
-# pruning score function 
+# pruning score function computed for every event based on the number of constraints specified 
 def pruningScore(query):
 	score = 0
 	# based on the number of constraints specified 
@@ -42,9 +40,16 @@ def queryScheduler(queries):
 	for query in queries:
 		# caluclate pruning score 
 		pruningScores[query] = pruningScore(query)
+	sortedScores = dict(sorted(pruningScores.items(), key=lambda item: item[1], reverse=True)) 		# Sort queries based on scores
 
-	dict(sorted(pruningScores.items(), key=lambda item: item[1]))
-	return pruningScores
+	# TO BE INTEGRATED WITH THE QUERY ENGINE CODE 
+	for query in sortedScores:
+		# change executeQuery function in QE to take in a query and return the results 
+		M[query] = executeQuery(query)					# map M that stores the mapping from the event pattern ID to the set of event ID tuples that its execution results belong to. 
+		executed.append(query)
+
+
+	return sortedScores
 
 def main():
 	testQueries = {"SELECT test FROM table WHERE a = 5 AND b = 7", "SELECT test2 FROM table WHERE a = 3", "SELECT test3 FROM table WHERE a = 1 AND b = 9 AND c = 12"}
