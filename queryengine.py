f80fc3fa0fb2036e00b56b60ac35a2d70a3db76c
Nicholas Meier <nlm15@my.fsu.edu>
2021-04-10 16:09:06 -0400

    updated query engine

--- queryengine.py
+++ queryengine.py
@@ -1,5 +1,36 @@
 import psycopg2
 from prettytable import PrettyTable
+from antlr4 import *
+import antlr4
+from aiqlLexer import aiqlLexer
+from aiqlParser import aiqlParser
+from aiqlListener import aiqlListener
+import sys
+
+class extendedListener(aiqlListener):
+	def enterAttr_cstr(self, ctx):
+		print(ctx.op())
+
+tokenList = []
+
+def generateQuery(tokens):
+	select = "SELECT * "
+	fromCondition = "FROM hostlogs "
+	where = "WHERE "
+
+	for i in range(0, len(tokens)):
+		print(tokens[i])
+
+		if str(tokens[i]) == "agentid":
+			where += "domainname=" + tokens[i+2]
+
+		if str(tokens[i]) == "from":
+			where += " AND time BETWEEN " + tokens[i+1] + " AND " + tokens[i+3]
+
+	query = select + fromCondition + where
+
+	return query
+
 
 def executeQuery():
 	conn = psycopg2.connect("dbname=projectdb user=postgres password=leoeatsbroccoli")
@@ -39,6 +70,11 @@ def main():
 	# parser = aiqlParser(stream)
 	# tree = parser.aiql()
 	# printNodes(tree)
+	# printer = extendedListener()	
+	# walker = ParseTreeWalker()
+	# walker.walk(printer, tree)
+
+
 	records = executeQuery()
 	printResults(records)
 
